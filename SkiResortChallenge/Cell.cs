0ed4d629e768b38784962ada2ab403ac6992bedf
Miguel Benitez Echenique <miguelbenitez159@hotmail.com>
2020-08-20 19:14:53 -0500

    Select best route

--- SkiResortChallenge/Cell.cs
+++ SkiResortChallenge/Cell.cs
@@ -8,6 +8,17 @@ namespace SkiResortChallenge
 {
     public class Cell
     {
+        public Cell()
+        {
+            PathDrop = -1;
+            Path = new List<int>();
+        }
+        public Cell(int pathDrop)
+        {
+            IsLoaded = true;
+            PathDrop = pathDrop;
+            Path = new List<int>() { pathDrop };
+        }
         public bool IsLoaded { get; set; }
         public int PathDrop { get; set; }
         public List<int> Path { get; set; }
--- SkiResortChallenge/SkiResortMatrix.cs
+++ SkiResortChallenge/SkiResortMatrix.cs
@@ -27,7 +27,7 @@ namespace SkiResortChallenge
                 {
                     if (!CellMatrix[i][j].IsLoaded)
                     {
-                        FindLongestRoute(new Coordinate(i, j), matrix, CellMatrix);
+                        FindLongestRoute(new Coordinate(i, j));
                     }
                     largestPath = CellMatrix[i][j].Path.Count > largestPath.Path.Count ?
                         CellMatrix[i][j] : largestPath;
@@ -62,7 +62,9 @@ namespace SkiResortChallenge
 
             CheckCellAllPosibleDirections(cdt, cellDirections);
 
-            return new Cell();
+            CellMatrix[cdt.X][cdt.Y] = GetBestRoute(cellDirections, Matrix[cdt.X][cdt.Y]);
+
+            return CellMatrix[cdt.X][cdt.Y];
         }
 
         private static Dictionary<string, Cell> GetCellsDirections()
@@ -83,24 +85,35 @@ namespace SkiResortChallenge
                 directions["up"] = CellMatrix[cdt.X][cdt.Y] = FindLongestRoute(new Coordinate(cdt.X, cdt.Y - 1));
             }
 
-            if (cdt.Y <Size- 1 && (Matrix[cdt.X][cdt.Y] > Matrix[cdt.X][cdt.Y + 1]))
+            if (cdt.Y < Size - 1 && (Matrix[cdt.X][cdt.Y] > Matrix[cdt.X][cdt.Y + 1]))
             {
                 directions["down"] = CellMatrix[cdt.X][cdt.Y] = FindLongestRoute(new Coordinate(cdt.X, cdt.Y + 1));
             }
 
             if (cdt.X > 0 && (Matrix[cdt.X][cdt.Y] > Matrix[cdt.X - 1][cdt.Y]))
             {
-                directions["left"] = CellMatrix[cdt.X][cdt.Y] = FindLongestRoute(new Coordinate(cdt.X-1, cdt.Y));
+                directions["left"] = CellMatrix[cdt.X][cdt.Y] = FindLongestRoute(new Coordinate(cdt.X - 1, cdt.Y));
             }
 
-            if (cdt.X <Size- 1 && (Matrix[cdt.X][cdt.Y] > Matrix[cdt.X + 1][cdt.Y]))
+            if (cdt.X < Size - 1 && (Matrix[cdt.X][cdt.Y] > Matrix[cdt.X + 1][cdt.Y]))
             {
                 directions["right"] = CellMatrix[cdt.X][cdt.Y] = FindLongestRoute(new Coordinate(cdt.X + 1, cdt.Y));
             }
         }
-        private static Cell GetBestRoute(Cell cell, int pathDrop)
+
+        private static Cell GetBestRoute(Dictionary<string, Cell> directions, int pathDrop)
         {
-            return new Cell();
+            var bestRoute = directions
+                .Where(c => c.Value.Path.Count > 0)
+                .OrderByDescending(c => c.Value.Path.Count)
+                .Select(c => (KeyValuePair<string, Cell>?)c)
+                .FirstOrDefault();
+
+            if (bestRoute?.Value is null) return new Cell(pathDrop);
+
+            var newCell = new Cell(pathDrop);
+            bestRoute?.Value.Path.ForEach(p => newCell.Path.Add(p));
+            return newCell;
         }
 
 
