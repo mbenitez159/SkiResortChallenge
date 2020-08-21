e4f77009a5b2ee4b6aea8aa465064affc5611f44
Miguel Benitez Echenique <miguelbenitez159@hotmail.com>
2020-08-20 21:50:22 -0500

    break the tie (get the steepest )

--- SkiResortChallenge/SkiResortMatrix.cs
+++ SkiResortChallenge/SkiResortMatrix.cs
@@ -30,9 +30,7 @@ namespace SkiResortChallenge
                 {
                     if (!CellMatrix[i][j].IsLoaded)
                         FindLongestRoute(new Coordinate(i, j));
-
-                    largestPath = CellMatrix[i][j].Path.Count > largestPath.Path.Count ?
-                        CellMatrix[i][j] : largestPath;
+                    largestPath = GetSteepestRoutePath(largestPath, CellMatrix[i][j]);
                 }
             }
             return largestPath;
@@ -129,6 +127,30 @@ namespace SkiResortChallenge
             return newCell;
         }
 
+        private static Cell GetSteepestRoutePath(Cell c1, Cell c2)
+        {
+            if (c1.Path.Count == c2.Path.Count)
+                return BrokeCellPathTie(c1, c2);
+
+            return c1.Path.Count > c2.Path.Count ?
+                        c1 : c2;
+        }
+
+        private static Cell BrokeCellPathTie(Cell c1, Cell c2)
+        {
+            Cell steepestCell = c1;
+            for (int i = 0; i < c1.Path.Count - 1; i++)
+            {
+                int c1PathSteep = c1.Path[i] - c1.Path[i + 1];
+                int c2PathSteep = c2.Path[i] - c2.Path[i + 1];
+                if (c1PathSteep != c2PathSteep)
+                {
+                    return c1PathSteep > c2PathSteep ?
+                        c1 : c2;
+                }
+            }
+            return steepestCell;
+        }
         #region Helper
         private static int[][] FilePathToIntMatrix(string filePath)
         {
